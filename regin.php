a4758f14880cdef2be6cf0e79da8752a5654194e
Kaktys868 <lastochka1230@yandex.ru>
2026-02-03 09:54:59 +0500

    шаг 4-5

--- regin.php
+++ regin.php
@@ -66,6 +66,33 @@
 				var _password = document.getElementsByName("_password")[0].value;
 				var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
 				
+				if(_login == "") {
+					alert("Введите логин!");
+					return;
+				}
+
+				if(_password == "") {
+					alert("Введите пароль!");
+					return;
+				}
+
+				if(CheckPassword(_password) == false) {
+					alert("Пароль должен содержать минимум 8 символов, заглавную и строчную буквы, цифру и специальный символ!");
+					return;
+				}
+
+				if(_password != _passwordCopy) {
+					alert("Пароли не совпадают!");
+					return;
+				}
+				loading.style.display = "block";
+				button.className = "button_diactive";
+
+				var data = new FormData();
+				data.append("login", _login);
+				data.append("password", _password);
+
+
 				if(_login != "") {
 					if(_password != "") {
 						if(_password == _passwordCopy) {
@@ -111,7 +138,10 @@
 					} else alert("Введите пароль.");
 				} else alert("Введите логин.");
 			}
-			
+			function CheckPassword(e){
+				let regex = /(?=.*[0-9])(?=.*[!@#$%^&*\-_=])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*\-_=]{8,}/;
+				return regex.test(value);
+			}
 			function PressToEnter(e) {
 				if (e.keyCode == 13) {
 					var _login = document.getElementsByName("_login")[0].value;
